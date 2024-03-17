4a3888046091bbc56501039cab2d794ddffe3de4
Tjan <tjan.kazar3@gmail.com>
2024-03-17 01:40:47 +0100

    centered div in novUporabnik

---
+++ Controllers/RegistracijaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSR_KAZAR_N1.Controllers
+{
+    public class RegistracijaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Naslov()
+        {
+            return View();
+        }
+        public IActionResult Zakljuci()
+        {
+            return View();
+        }
+        public IActionResult novUporabnik()
+        {
+            return View();
+        }
+
+    }
+}
--- Models/UporabnikModel.cs
+++ Models/UporabnikModel.cs
@@ -5,16 +5,32 @@
         public string ime { get; set; }
         public string priimek { get; set; }
         public DateTime datumRojstva { get; set; }
-        public string kontakt { get; set; }
+        public string emšo { get; set; }
+        public string naslov { get; set; }
+        public string email { get; set; }
+        public string geslo { get; set; }
         public List<Racun> racuni { get; set; }
 
-        public UporabnikModel(string ime, string priimek, DateTime datumRojstva, string kontakt, List<Racun> racuni)
+        public UporabnikModel(string ime, string priimek, DateTime datumRojstva, string email, List<Racun> racuni)
         {
             this.ime = ime;
             this.priimek = priimek;
             this.datumRojstva = datumRojstva;
-            this.kontakt = kontakt;
+            this.email = email;
             this.racuni = racuni;
         }
+
+        public UporabnikModel(string ime, string priimek, DateTime datumRojstva, string emšo, string naslov, string email, string geslo, List<Racun> racuni)
+        {
+            this.ime = ime;
+            this.priimek = priimek;
+            this.datumRojstva = datumRojstva;
+            this.emšo = emšo;
+            this.naslov = naslov;
+            this.email = email;
+            this.geslo = geslo;
+            this.racuni = racuni;
+        }
+        public UporabnikModel(){}
     }
 }
