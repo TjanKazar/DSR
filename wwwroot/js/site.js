f1143cb76b91e4845df413d8d8f1101aaa5fabed
TjanKazar <tjan.kazar3@gmail.com>
2024-03-18 18:53:44 +0100

    added error messages for user inputs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,45 @@
-﻿$(document).ready(function () {
-    $('.accordion-header').click(function () {
-        $(this).next('.accordion-content').slideToggle();
-        $('.accordion-content').not($(this).next('.accordion-content')).slideUp();
+﻿$(function () {
+    $("#accordion").accordion();
+});
+
+function isValidEMSO(emso) {
+    return /^\d{13}$/.test(emso);
+}
+
+function isValidPostNum(postnum) {
+    return /^\d{4}$/.test(postnum);
+}
+
+function PassIsEqual(pass1, pass2) {
+    return pass1 === pass2;
+}
+
+$(document).ready(function () {
+
+    $("#birthdate").datepicker();
+
+    $('#registration-form').submit(function (e) {
+        var emsoValue = $('#emso').val();
+        if (!isValidEMSO(emsoValue)) {
+            e.preventDefault();
+            $('#error-message-emso').show();
+        }
+    });
+
+    $('#naslov-form').submit(function (e) {
+        var postnumValue = $('#postnum').val();
+        if (!isValidPostNum(postnumValue)) {
+            e.preventDefault();
+            $('#error-message-post').show();
+        }
+    });
+
+    $('#zakljuci-form').submit(function (e) {
+        var pass1value = $('#pass1').val();
+        var pass2value = $('#pass2').val();
+        if (!PassIsEqual(pass1value, pass2value)) {
+            e.preventDefault();
+            $('#error-message-pass').show();
+        }
     });
 });
